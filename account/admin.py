21c59bb3533d062eb046b850a8ba397640ef6771
liu yuenfu <liuyuenfu@gmail.com>
2022-01-29 23:20:53 +0100

    imported the account.models into account.admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Account
 # Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email','first_name', 'last_name','date_joined', 'last_login', 'is_staff', 'is_admin')
+    search_fields = ('email',)
+    readonly_fields = ('id','date_joined', 'last_login',)
+# Register your models here.
+admin.site.register(Account,AccountAdmin)
