2f207ac7b0196c24b973727a2dd3ccd510819a5c
liu yuenfu <liuyuenfu@gmail.com>
2022-01-29 23:15:05 +0100

    added functions in the account.views file

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import SignupForm
 
 # Create your views here.
+def register(request):
+    form = SignupForm()
+    context = {"regform":form}
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return render(request,"index.html",context)
