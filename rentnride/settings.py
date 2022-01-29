82e949c0c8d4bf925a71de4a55e8be46293d055c
liu yuenfu <liuyuenfu@gmail.com>
2022-01-29 22:52:03 +0100

    created tailwind_theme app, account app

--- rentnride/settings.py
+++ rentnride/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'account',
+    'tailwind_theme',
 ]
 
 MIDDLEWARE = [
